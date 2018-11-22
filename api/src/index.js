f463ff103e2dbe32cfcea753272b45c2f3f8ef42
Shaun Sweet <shaun@shaunsweet.com>
2018-11-21 20:21:36 -0700

    add yoga graphql server

---
+++ api/src/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.start(
+  {
+    port: process.env.PORT,
+    cors: {
+      credentials: true,
+      origin: process.env.UI_URL,
+    },
+  },
+  (deets) => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  },
+);
